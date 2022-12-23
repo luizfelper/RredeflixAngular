70103320c384341ee188bf8a1070394ae1c37f4d
luizfelper <luizfelper@gmail.com>
2022-12-23 15:00:16 -0300

    Header active background Head when scroll Y > 10

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  @Input() black!: boolean;
+  backHeader: boolean = false;
+
+  constructor(){}
+
+  scrollListener = () => {
+    if(window.scrollY > 10) {
+      this.backHeader = true;
+      console.log(this.backHeader);
+    } else {
+      this.backHeader = false;
+      console.log(this.backHeader);
+    }
+  };
+
+  ngOnInit() {
+    this.scrollListener();
+    window.addEventListener('scroll', this.scrollListener);
+    return () => {
+      window.removeEventListener('scroll', this.scrollListener);
+    }
+  };
 
 }
