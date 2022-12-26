a34f566b45e32ae133a1392ce3e78eceb398538d
luizfelper <luizfelper@gmail.com>
2022-12-26 10:59:46 -0300

    Format document

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {FilmesService} from "../../../services/filmes.service";
+import { FilmesService } from '../../../services/filmes.service';
 import { Filmes } from 'src/app/Filmes';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   featureData: Filmes = {} as Filmes;
@@ -13,33 +13,33 @@ export class HomeComponent {
   movieList: any[] = [];
   firstDate: any;
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(private filmesService: FilmesService) {}
 
   async loadlAll() {
     let list = await this.filmesService.getHomeList();
     this.movieList = await this.filmesService.getHomeList();
     // Pegando o Filme em Destque e mostra na tela!!!
-    let originals: any = list.filter((i: any)=>i.slug === 'originals');
-    let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+    let originals: any = list.filter((i: any) => i.slug === 'originals');
+    let randomChosen = Math.floor(
+      Math.random() * (originals[0].items.results.length - 1)
+    );
     let chosen = originals[0].items.results[randomChosen];
     let chosenInfo = await this.filmesService.getMovieInfo(chosen.id, 'tv');
     this.featureData = chosenInfo;
     this.firstDate = new Date(chosenInfo.first_air_date);
-    
+
     if (chosenInfo.overview.length > 200) {
       this.description = chosenInfo.overview.substring(0, 200) + '...';
     } else {
       this.description = chosenInfo.overview;
     }
 
-    for(let i in chosenInfo.genres) {
-      this.genres.push(chosenInfo.genres[i].name)
+    for (let i in chosenInfo.genres) {
+      this.genres.push(chosenInfo.genres[i].name);
     }
-  };
+  }
 
   ngOnInit() {
     this.loadlAll();
   }
-
 }
- 
--- src/app/services/filmes.service.ts
+++ src/app/services/filmes.service.ts
@@ -6,10 +6,9 @@ import { lastValueFrom, Observable } from 'rxjs';
 import { Filmes, itemsHomeList, totalItems } from 'src/app/Filmes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilmesService {
-
   constructor(private http: HttpClient) {}
 
   private baseApiUrl: string = environment.baseApiUrl;
@@ -25,76 +24,96 @@ export class FilmesService {
 
   featureData: any = {};
 
-  
-//   async basicFetch(endpoint: string) {
-//     const request = await this.http.get(`${this.baseApiUrl}${endpoint}`);
-//     return request;
-//   }
+  //   async basicFetch(endpoint: string) {
+  //     const request = await this.http.get(`${this.baseApiUrl}${endpoint}`);
+  //     return request;
+  //   }
 
   async basicFetch(endpoint?: string): Promise<Object> {
     const request = await this.http.get(`${this.baseApiUrl}${endpoint}`);
     return await lastValueFrom(request);
   }
 
-
-  
   async getHomeList(): Promise<itemsHomeList[]> {
     return [
       {
-          slug: 'originals',
-          title: 'Originais do Netflix',
-          items: await this.basicFetch(`/discover/tv?${this.OriginaisNetflix}&${this.Idioma}&api_key=${this.apiKey}`)
+        slug: 'originals',
+        title: 'Originais do Netflix',
+        items: await this.basicFetch(
+          `/discover/tv?${this.OriginaisNetflix}&${this.Idioma}&api_key=${this.apiKey}`
+        ),
       },
       {
-          slug: 'trending',
-          title: 'Recomendados para você',
-          items: await this.basicFetch(`/trending/all/week?${this.Idioma}&api_key=${this.apiKey}`)
+        slug: 'trending',
+        title: 'Recomendados para você',
+        items: await this.basicFetch(
+          `/trending/all/week?${this.Idioma}&api_key=${this.apiKey}`
+        ),
       },
       {
-          slug: 'toprated',
-          title: 'Em Alta',
-          items: await this.basicFetch(`/movie/top_rated?${this.Idioma}&api_key=${this.apiKey}`)
+        slug: 'toprated',
+        title: 'Em Alta',
+        items: await this.basicFetch(
+          `/movie/top_rated?${this.Idioma}&api_key=${this.apiKey}`
+        ),
       },
       {
-          slug: 'action',
-          title: 'Ação',
-          items: await this.basicFetch(`/discover/movie?${this.generoAcao}&${this.Idioma}&api_key=${this.apiKey}`)
-      },            {
-          slug: 'comedy',
-          title: 'Comédia',
-          items: await this.basicFetch(`/discover/movie?${this.generoComedia}&${this.Idioma}&api_key=${this.apiKey}`)
-      },            {
-          slug: 'horror',
-          title: 'Terror',
-          items: await this.basicFetch(`/discover/movie?${this.generoTerror}&${this.Idioma}&api_key=${this.apiKey}`)
-      },            {
-          slug: 'romance',
-          title: 'Romance',
-          items: await this.basicFetch(`/discover/movie?${this.generoRomance}&${this.Idioma}&api_key=${this.apiKey}`)
-      },            {
-          slug: 'documentary',
-          title: 'Documentários',
-          items: await this.basicFetch(`/discover/movie?${this.Documentarios}&${this.Idioma}&api_key=${this.apiKey}`)
+        slug: 'action',
+        title: 'Ação',
+        items: await this.basicFetch(
+          `/discover/movie?${this.generoAcao}&${this.Idioma}&api_key=${this.apiKey}`
+        ),
       },
-  ];
+      {
+        slug: 'comedy',
+        title: 'Comédia',
+        items: await this.basicFetch(
+          `/discover/movie?${this.generoComedia}&${this.Idioma}&api_key=${this.apiKey}`
+        ),
+      },
+      {
+        slug: 'horror',
+        title: 'Terror',
+        items: await this.basicFetch(
+          `/discover/movie?${this.generoTerror}&${this.Idioma}&api_key=${this.apiKey}`
+        ),
+      },
+      {
+        slug: 'romance',
+        title: 'Romance',
+        items: await this.basicFetch(
+          `/discover/movie?${this.generoRomance}&${this.Idioma}&api_key=${this.apiKey}`
+        ),
+      },
+      {
+        slug: 'documentary',
+        title: 'Documentários',
+        items: await this.basicFetch(
+          `/discover/movie?${this.Documentarios}&${this.Idioma}&api_key=${this.apiKey}`
+        ),
+      },
+    ];
   }
 
   async getMovieInfo(movieId: number, type: string) {
-          let info: any = {};
-            if(movieId){
-                switch(type){
-                    case 'movie':
-                        info = await this.basicFetch(`/movie/${movieId}?${this.Idioma}&api_key=${this.apiKey}`);
-                        break;
-                        case 'tv':
-                            info = await this.basicFetch(`/tv/${movieId}?${this.Idioma}&api_key=${this.apiKey}`);
-                        break;
-                        default:
-                            info = null;
-                        break;
-                }
-            }
-        return info;
+    let info: any = {};
+    if (movieId) {
+      switch (type) {
+        case 'movie':
+          info = await this.basicFetch(
+            `/movie/${movieId}?${this.Idioma}&api_key=${this.apiKey}`
+          );
+          break;
+        case 'tv':
+          info = await this.basicFetch(
+            `/tv/${movieId}?${this.Idioma}&api_key=${this.apiKey}`
+          );
+          break;
+        default:
+          info = null;
+          break;
+      }
+    }
+    return info;
   }
-
 }
